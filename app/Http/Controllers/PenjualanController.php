7b929fb694d41d199748f482d41e3216c2e93f09
syakil <syakilahmadginanjar@gmail.com>
2021-02-22 14:48:45 +0700

    unit

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -19,116 +19,119 @@ class PenjualanController extends Controller{
 
   }
 
-   public function listData($awal,$akhir){
+  public function listData($awal,$akhir){
 
-        $branch = Branch::where('kode_gudang',Auth::user()->unit)->get();
-        $unit = array();
+    $branch = Branch::where('kode_gudang',Auth::user()->unit)->get();
+    $unit = array();
 
-        foreach($branch as $list){
-            $unit[] = $list->kode_toko;
-        }
+    foreach($branch as $list){
+        $unit[] = $list->kode_toko;
+    }
 
-        $user = DB::table('users')->whereIn('unit',$unit)->where('level',2)->get();
+    $user = DB::table('users')->whereIn('unit',$unit)->where('level',2)->get();
 
-        $id_user = array();
+    $id_user = array();
 
-        foreach ($user as $list ) {
-            $id_user [] = $list->id; 
-        }
+    foreach ($user as $list ) {
+        $id_user [] = $list->id; 
+    }
 
 
-        $penjualan_data = DB::table('penjualan')->whereIn('id_user',$id_user)->get();
+    $penjualan_data = DB::table('penjualan')->whereIn('id_user',$id_user)->get();
 
-        $id_penjualan = array();
+    $id_penjualan = array();
 
-        foreach($penjualan_data as $list){
-            $id_penjualan[] = $list->id_penjualan;
-        }
-     if ($awal === $akhir) {
+    foreach($penjualan_data as $list){
+      $id_penjualan[] = $list->id_penjualan;
+    }
     
-        $penjualan = Penjualan::leftJoin('users', 'users.id', '=', 'penjualan.id_user')
-        ->leftJoin('member','member.kode_member','=','penjualan.kode_member')
-        ->leftJoin('pabrik','member.CODE_KEL','=','pabrik.kode_pabrik')
-        ->select('users.*', 'penjualan.*', 'penjualan.created_at as tanggal','member.nama as nama_member','pabrik.kode_pabrik')
-        ->where('penjualan.created_at','LIKE', $awal.'%')
-        ->whereIn('id_penjualan',$id_penjualan)
-        ->orderBy('penjualan.id_penjualan', 'desc')
-        ->get();
+    if ($awal === $akhir) {  
+  
+      $penjualan = Penjualan::leftJoin('users', 'users.id', '=', 'penjualan.id_user')
+      ->leftJoin('member','member.kode_member','=','penjualan.kode_member')
+      ->leftJoin('pabrik','member.CODE_KEL','=','pabrik.kode_pabrik')
+      ->select('users.*', 'penjualan.*', 'penjualan.created_at as tanggal','member.nama as nama_member','pabrik.kode_pabrik')
+      ->where('penjualan.created_at','LIKE', $awal.'%')
+      ->whereIn('id_penjualan',$id_penjualan)
+      ->orderBy('penjualan.id_penjualan', 'desc')
+      ->get();
+    
+    }else {
       
-     }else {
-       
-       $penjualan = Penjualan::leftJoin('users', 'users.id', '=', 'penjualan.id_user')
-       ->leftJoin('member','member.kode_member','=','penjualan.kode_member')
-       ->leftJoin('pabrik','member.CODE_KEL','=','pabrik.kode_pabrik')
-       ->select('users.*', 'penjualan.*', 'penjualan.created_at as tanggal','member.nama as nama_member','pabrik.kode_pabrik')
-       ->whereBetween('penjualan.created_at',[$awal.'%',$akhir.'%'])
-->whereIn('id_penjualan',$id_penjualan)
-       ->orderBy('penjualan.id_penjualan', 'desc')
-       ->get();
-       
+      $penjualan = Penjualan::leftJoin('users', 'users.id', '=', 'penjualan.id_user')
+      ->leftJoin('member','member.kode_member','=','penjualan.kode_member')
+      ->leftJoin('pabrik','member.CODE_KEL','=','pabrik.kode_pabrik')
+      ->select('users.*', 'penjualan.*', 'penjualan.created_at as tanggal','member.nama as nama_member','pabrik.kode_pabrik')
+      ->whereBetween('penjualan.created_at',[$awal.'%',$akhir.'%'])
+      ->whereIn('id_penjualan',$id_penjualan)
+      ->orderBy('penjualan.id_penjualan', 'desc')
+      ->get();
+      
+    }
+
+    $penjualan = $penjualan->unique('id_penjualan');
+
+    $no = 0;
+    $data = array();
+
+    foreach($penjualan as $list){
+
+      if($list->kode_member == 0 || $list->kode_member == null){
+        $tipe_bayar = 'Cash';
+        $nama_member = '';
+      }else{
+        $tipe_bayar = 'Credit';
+        $nama_member = $list->nama_member;
       }
 
-$penjualan = $penjualan->unique('id_penjualan');
+      $no ++;
+      $row = array();
+      $row[] = $list->id_penjualan;
+      $row[] = $list->unit;
+      $row[] = tanggal_indonesia(substr($list->tanggal, 0, 10), false);
+      $row[] = $list->kode_pabrik;
+      $row[] = $list->kode_member;
+      $row[] = $nama_member;
+      $row[] = $list->total_item;
+      $row[] = $list->total_harga;
+      $row[] = $tipe_bayar;
+      $row[] = '<div class="btn-group">
+              <a onclick="deleteData('.$list->id_penjualan.')" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+              </div>';
+      $data[] = $row;
 
-     $no = 0;
-     $data = array();
+    }
 
-     foreach($penjualan as $list){
+    $output = array("data" => $data);
+    return response()->json($output);
 
-        if($list->kode_member == 0 || $list->kode_member == null){
-          $tipe_bayar = 'Cash';
-$nama_member = '';
-        }else{
-          $tipe_bayar = 'Credit';
-$nama_member = $list->nama_member;
-        }
+  }
 
-        $no ++;
-        $row = array();
-        $row[] = $list->id_penjualan;
-        $row[] = $list->unit;
-        $row[] = tanggal_indonesia(substr($list->tanggal, 0, 10), false);
-        $row[] = $list->kode_pabrik;
-        $row[] = $list->kode_member;
-        $row[] = $nama_member;
-        $row[] = $list->total_item;
-        $row[] = $list->total_harga;
-        $row[] = $tipe_bayar;
-        $row[] = '<div class="btn-group">
-                <a onclick="deleteData('.$list->id_penjualan.')" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
-                </div>';
-        $data[] = $row;
-
-      }
-
-     $output = array("data" => $data);
-     return response()->json($output);
-   }
-
-   public function show($id)
-   {
+  public function show($id){
    
-     $detail = PenjualanDetail::leftJoin('produk', 'produk.kode_produk', '=', 'penjualan_detail.kode_produk')
-        ->where('id_penjualan', '=', $id)
-        ->get();
+    $detail = PenjualanDetail::leftJoin('produk', 'produk.kode_produk', '=', 'penjualan_detail.kode_produk')
+      ->where('id_penjualan', '=', $id)
+      ->get();
      
     $no = 0;
     $data = array();
      
-      foreach($detail as $list){
-       $no ++;
-       $row = array();
-       $row[] = $list->kode_produk;
-       $row[] = $list->nama_produk;
-       $row[] = "Rp. ".format_uang($list->harga_jual);
-       $row[] = $list->jumlah;
-       $row[] = "Rp. ".format_uang($list->sub_total);
-       $data[] = $row;
-     }
+    foreach($detail as $list){
+      $no ++;
+      $row = array();
+      $row[] = $list->kode_produk;
+      $row[] = $list->nama_produk;
+      $row[] = "Rp. ".format_uang($list->harga_jual);
+      $row[] = $list->jumlah;
+      $row[] = "Rp. ".format_uang($list->sub_total);
+      $data[] = $row;
     
-     $output = array("data" => $data);
-     return response()->json($output);
-   }
+    }
+    
+    $output = array("data" => $data);
+    return response()->json($output);
+
+  }
    
    public function destroy($id)
    {
@@ -158,31 +161,31 @@ $nama_member = $list->nama_member;
    }
 
 
-   public function listDetail($awal,$akhir){
+  public function listDetail($awal,$akhir){
 
-$branch = Branch::where('kode_gudang',Auth::user()->unit)->get();
-        $unit = array();
+    $branch = Branch::where('kode_gudang',Auth::user()->unit)->get();
+    $unit = array();
 
-        foreach($branch as $list){
-            $unit[] = $list->kode_toko;
-        }
+    foreach($branch as $list){
+      $unit[] = $list->kode_toko;
+    }
 
-        $user = DB::table('users')->whereIn('unit',$unit)->where('level',2)->get();
+    $user = DB::table('users')->whereIn('unit',$unit)->where('level',2)->get();
 
-        $id_user = array();
+    $id_user = array();
 
-        foreach ($user as $list ) {
-            $id_user [] = $list->id; 
-        }
+    foreach ($user as $list ) {
+      $id_user [] = $list->id; 
+    }
 
 
-        $penjualan_data = DB::table('penjualan')->whereIn('id_user',$id_user)->get();
+    $penjualan_data = DB::table('penjualan')->whereIn('id_user',$id_user)->get();
 
-        $id_penjualan = array();
+    $id_penjualan = array();
 
-        foreach($penjualan_data as $list){
-            $id_penjualan [] = $list->id_penjualan;
-        }
+    foreach($penjualan_data as $list){
+      $id_penjualan [] = $list->id_penjualan;
+    }
 
     if ($awal == $akhir) {
 
@@ -203,19 +206,20 @@ $branch = Branch::where('kode_gudang',Auth::user()->unit)->get();
     }else {
       
       $penjualan = DB::table('penjualan_detail')->select(DB::raw('SUBSTR(penjualan.created_at,1,10) AS tanggal,
-      penjualan_detail.id_penjualan,
-      users.unit,
-      penjualan.kode_member as member,
-      member.nama,
-      penjualan_detail.kode_produk,
-      produk.nama_produk ,
-       penjualan_detail.harga_jual,
-       penjualan_detail.promo,
-       penjualan_detail.jumlah,
-       penjualan_detail.harga_beli,
-       sub_total,sub_total_beli,
-       penjualan_detail.id_penjualan_detail as detail
+        penjualan_detail.id_penjualan,
+        users.unit,
+        penjualan.kode_member as member,
+        member.nama,
+        penjualan_detail.kode_produk,
+        produk.nama_produk ,
+        penjualan_detail.harga_jual,
+        penjualan_detail.promo,
+        penjualan_detail.jumlah,
+        penjualan_detail.harga_beli,
+        sub_total,sub_total_beli,
+        penjualan_detail.id_penjualan_detail as detail
       '))
+
       ->leftJoin('penjualan','penjualan_detail.id_penjualan','penjualan.id_penjualan')
       ->leftJoin('member','penjualan.kode_member','member.kode_member')
       ->leftJoin('produk','penjualan_detail.kode_produk','produk.kode_produk')
@@ -228,43 +232,46 @@ $branch = Branch::where('kode_gudang',Auth::user()->unit)->get();
       
     }
 
-$penjualan = $penjualan->unique('detail');
+    $penjualan = $penjualan->unique('detail');
 
-      $no = 0;
-      $data = array();
-      foreach($penjualan as $list){
+    $no = 0;
+    $data = array();
 
-if($list->member == 0 || $list->member == null){
-          
-$nama_member = '';
-        }else{
-          
-$nama_member = $list->nama;
-        }
+    foreach($penjualan as $list){
 
-        $no ++;
-        $row = array();
-        $row[] = $list->tanggal;
-        $row[] = $list->unit;
-        $row[] = $list->id_penjualan;
-        $row[] = $list->member;
-        $row[] = $nama_member;
-        $row[] = $list->kode_produk;
-        $row[] = $list->nama_produk;
-        $row[] = $list->promo;
-        $row[] = $list->harga_jual;
-        $row[] = $list->harga_beli;
-        $row[] = $list->jumlah;
-        $row[] = $list->sub_total;
-        $row[] = $list->sub_total_beli;
-        $data[] = $row;
-
+      if($list->member == 0 || $list->member == null){
+                
+        $nama_member = '';
+      
+      }else{                
+      
+        $nama_member = $list->nama;
+      
       }
+
+      $no ++;
+      $row = array();
+      $row[] = $list->tanggal;
+      $row[] = $list->unit;
+      $row[] = $list->id_penjualan;
+      $row[] = $list->member;
+      $row[] = $nama_member;
+      $row[] = $list->kode_produk;
+      $row[] = $list->nama_produk;
+      $row[] = $list->promo;
+      $row[] = $list->harga_jual;
+      $row[] = $list->harga_beli;
+      $row[] = $list->jumlah;
+      $row[] = $list->sub_total;
+      $row[] = $list->sub_total_beli;
+      $data[] = $row;
+
+    }
      
-      $output = array("data" => $data);
-      return response()->json($output);
+    $output = array("data" => $data);
+    return response()->json($output);
  
-   }
+  }
    
 }
   
